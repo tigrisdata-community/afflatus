4d3f4e9578b0f24049b349984ded5ac5827ce7eb
Xe Iaso <xe@tigrisdata.com>
2025-11-26 11:03:56 -0500

    feat: implement fake search logic
    Signed-off-by: Xe Iaso <xe@tigrisdata.com>

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect
 from flask_htmx import HTMX
+from time import sleep
 
 
 app = Flask(__name__)
 htmx = HTMX(app)
+
+
+def simple_page(name):
+    if htmx:
+        return render_template(f"partials/{name}.html")
+    return render_template(f"pages/{name}.html")
 
 
 @app.route("/favicon.ico")
@@ -23,6 +30,36 @@ def healthz():
 
 @app.route("/")
 def index():
-    if htmx:
-        return render_template("partials/index_content.html")
-    return render_template("pages/index.html")
+    return simple_page("index")
+
+
+@app.route("/search")
+def search():
+    return simple_page("search")
+
+
+@app.route("/api/search", methods=["POST"])
+def api_search():
+    if not htmx:
+        return redirect("/search")
+
+    # Search pixeltable
+    sleep(2)
+
+    return render_template(
+        "partials/api/search.html",
+        results=[
+            {
+                "id": "image1",
+                "url": "https://files.xeiaso.net/hero/puppy-bunky.jpg",
+            },
+            {
+                "id": "image2",
+                "url": "https://files.xeiaso.net/hero/dgx-spark.jpg",
+            },
+            {
+                "id": "image3",
+                "url": "https://files.xeiaso.net/hero/around-the-bend.jpg",
+            },
+        ],
+    )
